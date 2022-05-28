cacb401f41df93e9fe09c6d3b3117a07a0c8d447
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-05-27 20:20:15 -0600

    Uninstalled 'Jade' server engine and updated express

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send('Welcome earthlings to project 3!!');
 });
 
 module.exports = router;
