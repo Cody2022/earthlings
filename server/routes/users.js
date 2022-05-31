6cf977f4fbdbc2045da712fc772cc74a10980e9b
Cody2022 <cody.zheng@hotmail.com>
2022-05-31 00:00:02 -0600

    Cody_added update by email endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
+var debug = require("debug")("server:routes");
 
-const { createUser, findByEmail } = require("../model/userModel");
+const { createUser, findByEmail, updateByEmail } = require("../model/userModel");
 
 /* GET users listing. */
 router.get("/", function (req, res) {
@@ -16,7 +17,7 @@ router.post("/signup", async (req, res) => {
   if (existingUser) {
     return res.send({
       status: "userexists",
-      message: "Username already taken! Please signup with a different name",
+      message: "Email already taken! Please signup with a different email",
     });
   } else if (email && password) {
     const createdNewUser = createUser(req.body);
@@ -28,9 +29,26 @@ router.post("/signup", async (req, res) => {
   } else {
     return res.send({
       status: "noInput",
-      message: "Enter username and a password",
+      message: "Enter email and a password",
     });
   }
 });
+
+router.put("/update/:email", async(req, res)=>{
+      console.log("req.body", req.body);
+      const email=req.params.email;
+      const updateData=req.body;
+  try{
+    let updatedUser=await updateByEmail(email, updateData);
+    if (updatedUser) {
+      res.send(updatedUser)
+  }
+ }catch(err){
+  debug(`failed to edit user with email: ${email}`);
+  debug(err.message);
+  res.status(500).send("Error in data. Please try again.");
+ }
+})
+
 
 module.exports = router;
