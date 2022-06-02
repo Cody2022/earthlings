122d22d47708142c27889219c5fb5ee4c6c4f563
Edmund Lee <edmundleekc@gmail.com>
2022-06-02 16:32:39 -0600

    added login, findAllUser, findByEmail, getNewComer and getVolunteer endpoints

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,6 +1,7 @@
 debug = require("debug")("server:userModel");
 const mongoose = require("./mongoose");
 const bcrypt = require("bcrypt");
+const { query } = require("express");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -15,7 +16,10 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 const createUser = async (newUserData) => {
-  const hashedPassword = bcrypt.hashSync(newUserData.password, (saltRounds = 10));
+  const hashedPassword = bcrypt.hashSync(
+    newUserData.password,
+    (saltRounds = 10)
+  );
   const newUser = await User.create({
     ...newUserData,
     password: hashedPassword,
@@ -31,36 +35,44 @@ const findByEmail = async (email) => {
     debug("Cannot find the email in database");
   }
 };
-// const findByEmail = async (email) => {
-//   try {
-//     let FoundByEmail = await User.findOne(email);
-//     return FoundByEmail;
-//   } catch (error) {
-//     console.log("Cannot find the username in database");
-//   }
-// };
-
-const updateByEmail = async (email, newUserData) => {
-  console.log("userModel.js email", email)
-  if (newUserData.password) {
-      const hashedPassword = bcrypt.hashSync(newUserData.password, (saltRounds = 10));
-      let updatedUser = await User.findOneAndUpdate({email: email}, {...newUserData, password: hashedPassword}, {
-      new: true,
-      });
-      return updatedUser;
-  } else {
-      let updatedUser = await User.findOneAndUpdate({email: email}, newUserData, {
-      new: true, upsert: true
-      });
-      return updatedUser;
-   }
-};
-
 const findUserById = async (id) => {
   let user = await User.findById(id);
   return user;
 };
 
+const updateByEmail = async (email, newUserData) => {
+  console.log("userModel.js email", email);
+  if (newUserData.password) {
+    const hashedPassword = bcrypt.hashSync(
+      newUserData.password,
+      (saltRounds = 10)
+    );
+    let updatedUser = await User.findOneAndUpdate(
+      { email: email },
+      { ...newUserData, password: hashedPassword },
+      {
+        new: true,
+      }
+    );
+    return updatedUser;
+  } else {
+    let updatedUser = await User.findOneAndUpdate(
+      { email: email },
+      newUserData,
+      {
+        new: true,
+        upsert: true,
+      }
+    );
+    return updatedUser;
+  }
+};
+
+// get all newcomers or volunteers based on filter
+const getAllUsers = async (filter) => {
+  const users = await User.find(filter);
+  return users;
+};
 
 //   Update exiting user data
 const updateUserById = async (id, newUserData) => {
@@ -80,6 +92,7 @@ module.exports = {
   createUser,
   findByEmail,
   updateByEmail,
+  getAllUsers,
   findUserById,
   updateUserById,
   deleteUserById,
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
-var express = require("express");
+const express = require("express");
 const { get } = require("mongoose");
-var router = express.Router();
+const router = express.Router();
 var debug = require("debug")("server:users");
+const bcrypt = require("bcrypt")
 
-const { createUser, findByEmail, updateByEmail } = require("../model/userModel");
+const {
+  createUser,
+  findByEmail,
+  updateByEmail,
+  getAllUsers,
+} = require("../model/userModel");
 
 /* GET users listing. */
 router.get("/", function (req, res) {
@@ -36,20 +42,85 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.put("/update/:email", async(req, res)=>{
-  const email=req.params.email;
-  const updateData=req.body;
-  try{
-    let updatedUser=await updateByEmail(email, updateData);
+router.put("/update/:email", async (req, res) => {
+  const email = req.params.email;
+  const updateData = req.body;
+  try {
+    let updatedUser = await updateByEmail(email, updateData);
     if (updatedUser) {
-     res.send(updatedUser)
+      res.send(updatedUser);
+    }
+  } catch (err) {
+    debug(`failed to edit user with email: ${email}`);
+    debug(err.message);
+    res.status(500).send(`account of ${email} cannot be updated`);
   }
- }catch(err){
-  debug(`failed to edit user with email: ${email}`);
-  debug(err.message);
-  res.status(500).send(`account of ${email} cannot be updated`);
- }
+});
+
+// getUserByEmail
+router.get("/get/:email", async (req, res) => {
+  const email = req.params.email;
+
+  debug(`:email is (${email})`);
+  try {
+    let userFound = await findByEmail({email: email});
+    if (userFound) {
+      res.send (userFound)
+    }
+      } catch (err) {
+        debug(`failed to find user with email: ${email}`);
+        debug(err.message);
+        res.status(500).send(`account of ${email} cannot be found`);
+  }
+});
+
+// get newcomers
+router.get("/getnewcomers", async(req, res)=>{
+  let filter={isNewcomer:true}
+  let newcomers=await getAllUsers(filter);
+  res.send(newcomers);
 })
+
+// get Volunteers
+router.get("/getvolunteers", async(req, res)=>{
+  let filter={isVolunteer:true}
+  let volunteers=await getAllUsers(filter);
+  res.send(volunteers);
+})
+
+router.post("/login", async (req,res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email === undefined || password === undefined) {
+    return res.send({
+      status: "noInput",
+      id: email,
+      message: "Enter email and password please",
+    });
+  }
+  const userInfo = await findByEmail({ email: email });
+  if (userInfo === null) {
+    res.send({
+      status: "notSignup",
+      id: email,
+      message: "New user, Signup first please",
+    });
+  } else {
+    const result = await bcrypt.compare(password, userInfo.password);
+    if (result === true) {
+      res.send(userInfo);
+    } else {
+      res.send({
+        status: "failed",
+        id: email,
+        message: "Wrong email or password",
+      });
+    }
+  }
+})
+
+
+
 
 
 module.exports = router;
