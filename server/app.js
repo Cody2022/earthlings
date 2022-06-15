978be681e2e664176e71abee1f9deb5080d6dcc9
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-15 15:45:09 -0600

    installed socket.io dependencies and set up socket io to send and receive message to client side

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const http = require("http");
+const cors = require("cors");
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+app.use(cors());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +22,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Set up socketio variable to emit and receive messages to clients
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected with: ${socket.id}`);
+
+  //Allow socket.io to join a room
+  socket.on("join-room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  //Create a socket.io event to send the message to the server
+  socket.on("send-message", (data) => {
+    console.log(`Sending message data`)
+    socket.timeout(data.room).emit("receive-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected with id: ${socket.id}`);
+  });
+});
 
 app.use('/', usersRouter);
 
