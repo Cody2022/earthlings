0c194b8766dc7c4b5ace17b7443bfe7915b76c8c
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-21 15:46:56 -0600

    Able to create a new conversation, find all members chats

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { createMembers } = require('../model/conversationModel');
+const { createMembers, findConversation} = require('../model/conversationModel');
 
+//Create a new conversation
 router.post("/", async (req, res) => {
-    const newMembers = req.body;
+    const newMembers = { members: [req.body.members.senderId, req.body.members.recieverId] };
     try {
         const addedMembers = await createMembers(newMembers);
         console.log(`Adding members to conversation with id of: ${addedMembers}`);
         res.json(newMembers);
     } catch (err) {
         res.status(500).json(err.message);
+    }
+});
+
+//Get the conversation from ID
+router.get("/:senderId", async (req, res) => {
+    const id = req.params.senderId;
+    try {
+        const getConversation = await findConversation(id);
+        console.log(`Retrived conversation with id of: ${id}`);
+        res.send(getConversation);
+    } catch (err) {
+        res.status(500).send(err.message);
     }
 });
 
