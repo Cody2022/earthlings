254c097e920bce8d4bcdcd3a3331d741504b754a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-23 19:50:55 -0600

    Change 'get user' to by email

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -4,7 +4,7 @@ const { createMembers, findConversation} = require('../model/conversationModel')
 
 //Create a new conversation
 router.post("/", async (req, res) => {
-    const newMembers = { members: [req.body.members.senderId, req.body.members.recieverId] };
+    const newMembers = { members: [req.body.members.senderEmail, req.body.members.recieverEmail] };
     try {
         const addedMembers = await createMembers(newMembers);
         console.log(`Adding members to conversation with id of: ${addedMembers}`);
@@ -15,8 +15,8 @@ router.post("/", async (req, res) => {
 });
 
 //Get the conversation from ID
-router.get("/:senderId", async (req, res) => {
-    const id = req.params.senderId;
+router.get("/:senderEmail", async (req, res) => {
+    const id = req.params.senderEmail;
     try {
         const getConversation = await findConversation(id);
         console.log(`Retrived conversation with id of: ${id}`);
