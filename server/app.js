5210ac033097135aa389f090e680900457f25cb8
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-29 23:01:50 -0600

    Added a new file and added real-time chat

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+// const logger = require("morgan");
 
 const usersRouter = require("./routes/users");
 const membersConversationRoute = require("./routes/conversations");
@@ -16,7 +16,7 @@ app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
