67471a64748aff642d17b347e1fbc2c61f70f40a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-07-05 12:39:00 -0600

    app.js now pointing to donation router

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 // const logger = require("morgan");
 
+const donationRouter = require("./routes/donations")
 const usersRouter = require("./routes/users");
 const membersConversationRoute = require("./routes/conversations");
 const messagesRoute = require("./routes/messages");
@@ -34,6 +35,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/donations", donationRouter);
 app.use("/", usersRouter);
 app.use("/conversation", membersConversationRoute);
 app.use("/messages", messagesRoute);
